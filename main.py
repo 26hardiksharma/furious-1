be64d4343cf77dcdd1f0f59e068e72d6af91c7ff
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-02-20 10:45:29 +0530

    commit on main

--- main.py
+++ main.py
@@ -1619,6 +1619,6 @@ async def on_guild_role_create(role):
     ch = client.get_channel(812547608593694740)
     embed = discord.Embed(title= "Role Created",colour = 0xFF0000)
     embed.add_field(name= "Role Info",value = f"Mention :- {role.mention}\nColor :- {role.color}\nHoisted :- {role.hoist}\nMentionable :- {role.mentionable}")
-    embed.set_footer(text = f"Role ID :- {role.id} • Today At {datetime.utcnow}")
+    embed.set_footer(text = f"Role ID :- {role.id} • Today At {datetime.datetime.utcnow}")
     await ch.send(embed=embed)
 client.run(TOKEN)
