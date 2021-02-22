834d53744e87d2edc58371b0d96d9085a36dc366
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-02-22 21:14:51 +0530

    commit on main

--- new.py
+++
@@ -1,6 +0,0 @@
-import discord
-from discord.ext import commands
-client = commands.Bot(command_prefix =["^","furious ","<@!790478502909837333> "],help_command=None,case_insensitive = True)
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello")
