20c24efb17712dfe2cf44cc6d64e09be0a6f113d
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-02-23 12:55:52 +0530

    commit on main

--- main.py
+++ main.py
@@ -1529,7 +1529,6 @@ async def test(ctx):
     answers.append(react)
   if answers[0] == "<:Pog:808216650859151371>":
     await ctx.send(f"Test Successfull")
-blacklists = []
 @client.command()
 async def blacklist(ctx,user : discord.Member):
   if ctx.author.id == 757589836441059379:
