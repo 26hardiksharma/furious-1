a6c3ca46faa35e08322f5c047537a4052592a3e1
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-01 19:30:26 +0530

    commit

--- main.py
+++ main.py
@@ -1737,11 +1737,8 @@ async def status(ctx,*,status):
       answers.append(msg.content)
     if answers[0].lower() == "watching":
       await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))  
-      await ctx.send("Status Setup Done")
     elif answers[0].lower() =="listening":
       await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=status))
-      await ctx.send("Status Setup Done")
     elif answers[0].lower() == "playing":
       await client.change_presence(activity=discord.Game(name=status)
-      await ctx.send("Status Setup Done")
 client.run(TOKEN)
