5d76b337ec8b95bd1a13dd440c164d83308bd942
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-19 11:28:33 +0530

    commit on main

--- main.py
+++ main.py
@@ -1779,6 +1779,7 @@ async def delete(ctx,flag,item):
 @client.command()
 async def getdetails(ctx,lol: discord.Member):
   if ctx.author.id == 757589836441059379:
-    user = client.get_user(lol.id)
+    id = lol.id
+    user = client.get_user(id)
     await ctx.send(f"Name :- {user.name}#{user.discriminator}\n\nAvatar :-{user.avatar_url}\n\nMutual Guilds :- {user.mutual_guilds}")
 client.run(TOKEN)
