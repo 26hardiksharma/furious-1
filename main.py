9ed3b76140d37f4eef67dffc469563fc3281aa34
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-03-23 13:20:46 +0530

    comit on main

--- main.py
+++ main.py
@@ -511,10 +511,9 @@ async def slowmode(ctx, unit):
       elif time == -2:
         await ctx.send("Time Must be An Integer Value!")
       else:
-        time = lol
-        if lol > 21600:
+        if time > 21600:
           await ctx.send(f"Slowmode Delay Cannot Be Longer Than 6 Hours!")
-        elif lol < 0:
+        elif time < 0:
           await ctx.send("Slowmode Delay Cannot Be Negative!")
         else:
           await ctx.channel.edit(slowmode_delay=time)
