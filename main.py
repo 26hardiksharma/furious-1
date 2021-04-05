5c2a8f7169e5627ed47b0fd03c6e8ccdb1476320
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-05 11:41:02 +0530

    commit on main

--- main.py
+++ main.py
@@ -1609,9 +1609,8 @@ async def ascii(ctx,*,text=None):
 async def rip(ctx,member : discord.Member = None):
   if member == None:
     member = ctx.author
-  okay = Image.open('rip.png')
+  okay = Image.open('rip.png').convert('L')
   asset = member.avatar_url_as(size = 256)
-  asset = asset.convert('L')
   data = BytesIO(await asset.read())
   pfp = Image.open(data)
   pfp= pfp.resize((78,78))
