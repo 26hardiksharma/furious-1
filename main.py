aaf962ad3a9a218dde44ebd302947fa3ed5debac
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-08 17:47:12 +0530

    commit on main

--- main.py
+++ main.py
@@ -1764,6 +1764,6 @@ async def prefix(ctx,prefix = None):
     if prefix == None:
       await ctx.send("Please Be Sure To Supply The Prefix You Want To Be Set For This Server While Using This Command!")
       return
-    await client.config.insert_one({"_id": ctx.guild.id,"prefix":prefix})    
+    await client.db.insert_one({"_id": ctx.guild.id,"prefix":prefix})    
     await ctx.send(f'The New Prefix Was Set To `{prefix}` ;)')
 client.run(TOKEN)
