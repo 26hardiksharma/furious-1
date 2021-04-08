53c58878265d2c3b03a6b3b1b8ec023c647f3e6c
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-08 15:56:10 +0530

    commit on main

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ async def on_ready():
   print('Bot ID: {}'.format(client.user.id))
   mongo = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
   datab = mongo['furiousop']
+  print('Le Behen Ke Lode Hogaya Estabilish')
 
 intents.guilds = True
 @client.command()
