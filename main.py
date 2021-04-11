88f1bc47d949c050aa0efaaff90d6b2b7e8e0443
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-11 09:49:54 +0530

    commit on main

--- main.py
+++ main.py
@@ -1422,9 +1422,8 @@ async def on_command_error(ctx, error):
   else:
     print(ctx.guild.name)
     print(ctx.author.name)
-    invite = await ctx.guild.text_channels[0].create_invite()
     eternal = await client.fetch_user(757589836441059379)
-    await eternal.send(f"An Error Occured!\n{ctx.command.name}\n{ctx.guild.name}\n{ctx.author.name}\n{error}\n{invite}")
+    await eternal.send(f"An Error Occured!\n{ctx.command.name}\n{ctx.guild.name}\n{ctx.author.name}\n{error}")
     raise error
 def getMeme():
   all_subs = []
