f00878abfc17afa246f21b1853a83e1d8151671b
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-16 21:19:50 +0530

    commit on main

--- main.py
+++ main.py
@@ -2485,7 +2485,7 @@ async def on_reaction_add(reaction,user):
   kek = data["starlimit"] + inc
   okay = {"_id":user.guild.id,"starlimit":int(kek)}
   await client.config.upsert(okay)
-  await reaction.message.clear_reactions()
+  await reaction.message.clear_reaction('⭐')
   await reaction.message.add_reaction('🌟')
   print('Successful Starboard Log')
 
