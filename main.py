b023f1422f36d93d593f4459c99edc788ec4492f
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-16 11:35:23 +0530

    commit on main

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def getprefix(client,message):
 client = commands.Bot(command_prefix =getprefix,help_command=None,case_insensitive = True)
 
 
-client.dblpy = dbl.DBLClient(client,dbl_token,webhook_port = 5000)
+client.dblpy = dbl.DBLClient(client,dbl_token,webhook_path = '/dblwebhook',webhook_auth = 'pogchamp',webhook_port = 5000)
 
 
 intents = discord.Intents.default()
