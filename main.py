4b846dc10f3d18ef8de9e60da0d7e8f011b0c3a5
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-19 20:08:11 +0530

    commit on main

--- main.py
+++ main.py
@@ -2653,7 +2653,7 @@ async def addemoji(ctx,name = None):
       return await ctx.send('You Must Specify A Name For The Emoji!')
     if not ctx.message.attachments:
       return await ctx.send('Please Provide An image To Be Uploaded As An Emoji!')
-    bytes = message.attachments[0].getvalue()
+    bytes = ctx.message.attachments[0].getvalue()
     emoji = await ctx.guild.create_custom_emoji(name = name,image = bytes)
-    await ctx.send(f'Created Emoji {emoji}')
+    await ctx.send(f'Created Emoji <:{emoji.name}:{emoji.id}>')
 client.run(TOKEN)
