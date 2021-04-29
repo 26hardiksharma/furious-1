06b6a7d37bc3363fa543e0996745c6595ba45b6a
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-04-29 13:49:03 +0530

    commit on main

--- main.py
+++ main.py
@@ -2899,6 +2899,7 @@ async def security(ctx,query = None):
     embed.add_field(name = "About",value = f"Security Service Helps You Keep A Watch On The Hostile Activites Going Around In Your Server!",inline = False)
     embed.add_field(name = f"Information",value = f"If The @everyone Role Of The Server Is Granted Any Moderation Permissions, It Would Be Automatically Turned Off And The Moderator Will Be Notified!",inline = False)
     embed.add_field(name = "Methods",value = f"• **`F!setmodrole <@role>`**\n• **`F!security <on/off>`**")
+    await ctx.send(embed=embed)
     return
   if query.lower() == "on":
     if not ctx.author.guild_permissions.manage_guild:
