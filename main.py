a9f120fa6febfdd633c7b6651f1973cbfe028389
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-05-01 11:08:29 +0530

    commit on main

--- main.py
+++ main.py
@@ -2897,7 +2897,7 @@ async def on_guild_role_update(before,after):
     if not channel:
       return
     if len(hostile_perms) > 1:
-      await channel.send(content = f"<@&{data['modrole']}>\n{user} Granted The Everyone Role These Moderation Permissions:\n{hostile_perms}")
+      await channel.send(content = f"<@&{data['modrole']}>\n{user} Granted The **`@everyone`** Role These Moderation Permissions:\n{hostile_perms}")
 @client.command()
 async def setmodrole(ctx,role: discord.Role = None):
   if ctx.author.guild_permissions.manage_guild:
