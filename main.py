cf08963d3a4858dea48594751880ad725e4bf194
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-05-03 11:48:49 +0530

    commit on main

--- main.py
+++ main.py
@@ -2951,6 +2951,7 @@ async def on_bulk_message_delete(messages):
   msg = await channel.send(file = discord.File("delmsgs.txt"))
   url = msg.attachments[0].url[39:-4]
   uploadurl = f"https://txt.discord.website/?txt={url}"
+  print(uploadurl)
   embed = discord.Embed(description = f"{len(messages)} Messages Were Purged In {messages[0].channel.mention}\n\n [📱 View](uploadurl) [📥 Download]({msg.attachments[0].url})",timestamp = datetime.datetime.now(),color = messages[0].author.color)
   await log.send(embed=embed)
 client.run(TOKEN)
