63d554263316a8b8a1b4725dfe0ac27e71bcb397
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-05-06 18:24:59 +0530

    commit on main

--- main.py
+++ main.py
@@ -2987,6 +2987,7 @@ async def attachmentfilter(ctx,query = None,desc = None):
     embed.add_field(name = "About",value = f"Attachment Filter Is An Effective Method Of Filtering Out All Malicious Attachments And Files Which Users Send Over Discord.",inline = False)
     embed.add_field(name ="How It Works ?",value = f"Once Turned On, Attachment Filter Will Check For All Files And Attachments In A Message.\n\nIf Any Attachment Of A Malicious Format Is Found, The Bot Will Immediately Delete The Message And Perform An Action On The Sender **`[Configurable]`**",inline = False)
     embed.add_field(name = "Ignored Formats",value = f"**`png`**, **`jpg`**,**`jpeg`**,**`webp`**,**`mp3`**,**`mp4`**")
-    embed.add_field(name = "Usage",value = f"**`F!attachmentfilter <on/off>`**\n**`F!attachmentfilter action <kick/ban/mute>`**")
+    embed.add_field(name = "Usage",value = f"**`F!attachmentfilter <on/off>`**\n**`F!attachmentfilter action <kick/ban/mute>`**",inline = False)
+    
     await ctx.send(embed=embed)
 client.run(TOKEN)
