0af2dbfcae509c55fc078516f1f44d6dcc15a5a5
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-05-13 09:45:02 +0530

    commit on main

--- main.py
+++ main.py
@@ -176,10 +176,10 @@ async def whois(ctx, member : discord.Member = None):
       count += 1
   if rc >= 1:
     if len(role_str) <= 1024:
-      embed.add_field(name=f"Roles[{rc}]", value=role_str,inline = False)
+      embed.add_field(name=f"Roles[{rc}]", value=role_str)
     else:
-      embed.add_field(name = f"Roles[{rc}]",value = "User Has Too Many Roles!",inline = False)
-    embed.add_field(name="Highest Role:", value=member.top_role.mention,inline = False)
+      embed.add_field(name = f"Roles[{rc}]",value = "User Has Too Many Roles!")
+    embed.add_field(name="Highest Role:", value=member.top_role.mention)
   else:
     embed.add_field(name = "Roles",value = "None",inline = False)
   if count >= 1:
