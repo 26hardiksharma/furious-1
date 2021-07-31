5f40f121a73c8571b40cee50f05692416600182b
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2021-07-31 08:35:56 +0530

    commit on main

--- main.py
+++ main.py
@@ -849,7 +849,7 @@ async def help(ctx,query = None):
         pag.add_reaction(i,dict[i])
       embeds = [embed1,embed2,embed3,embed4,embed5]
       await pag.run(embeds)"""
-      msg =await ctx.send(embed=embed1,components = [Select(placeholder = "Select A Help Option To Paginate To",options = [SelectOption(label = "Moderation",value = '2'),SelectOption(label = 'Fun',value = '4'),SelectOption(label = 'Utility',value = '4'),SelectOption(label = 'Management',value = '4')])])
+      msg =await ctx.send(embed=embed1,components = [Select(placeholder = "Select A Help Option To Paginate To",options = [SelectOption(label = "Moderation",value = 'a'),SelectOption(label = 'Fun',value = 'b'),SelectOption(label = 'Utility',value = 'c'),SelectOption(label = 'Management',value = 'd')])])
       o = await client.wait_for('select_option',check = lambda m: i.message.id ==msg.id and i.user == ctx.author)
       if o.component[0].label.lower() == "Fun":
         await msg.edit(embed = embed2)
