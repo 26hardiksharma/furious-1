eeb3da86fa9abeae92f24bda27759d7f4b0b0d41
EternalSlayer <74042237+EternalSlayer@users.noreply.github.com>
2022-08-21 22:45:13 +0530

    commit

--- main.py
+++ main.py
@@ -851,9 +851,9 @@ async def help(ctx,query = None):
         pag.add_reaction(i,dict[i])
       embeds = [embed1,embed2,embed3,embed4,embed5]
       await pag.run(embeds)"""
-      msg =await ctx.send(embed=embed1,components = [Select(placeholder = "Select A Help Option To Paginate To",options = [SelectOption(label = "Moderation",value = 'a'),SelectOption(label = 'Fun',value = 'b'),SelectOption(label = 'Utility',value = 'c'),SelectOption(label = 'Management',value = 'd')])])
-      o = await client.wait_for('select_option',check = lambda i: i.message.id ==msg.id and i.user == ctx.author)
-      if o.component[0].label.lower() == "Fun":
+      msg =await ctx.send(content='Kindly use the buttons to paginate through the embed.',embed=embed1,components = [Button(label='Moderation'),Button(label="Management"),Button(label='utulity'),Button(label='Fun')])
+      o = await client.wait_for('button_click',check = lambda i: i.message.id ==msg.id and i.user == ctx.author)
+      if o.component[0].label.lower() == "fun":
         await msg.edit(embed = embed2)
       elif o.component[0].label.lower() == "moderation":
         await msg.edit(embed = embed3)
